decde8541a17a7d1acb60582eed40847e54fe1d2
Kailash Baas <kailash.baas@gmail.com>
2018-02-13 10:57:14 -0800

    Added functionality for adding and removing potential and accepted passengers

--- app/src/main/java/chub45/benson/hitch/Post.java
+++ app/src/main/java/chub45/benson/hitch/Post.java
@@ -20,9 +20,17 @@ public interface Post
     public String getauthor_email();
     public Uri getauthor_profile_pic();
     public String getdescription();
+    public String getpotential_passengers();
+    public String getaccepted_passengers();
     public void set_departing_area(String departing_area);
     public void set_destination(String destination);
     public void set_departure_time(Date time);
     public void set_available_spots(int size);
     public void set_description(String description);
+    public void setpotential_passengers(String potential_passengers);
+    public void setaccepted_passengers(String accepted_passengers);
+    public void add_potential_passenger(String passenger);
+    public void add_accepted_passenger(String passenger);
+    public void remove_potential_passenger(String passenger);
+    public void remove_accepted_passenger(String passenger);
 }
