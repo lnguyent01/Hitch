694768f67f5c8f7659775c1c127949c40244e798
Matthew Mitchell <matthewmitchell@umail.ucsb.edu>
2018-02-21 01:39:01 -0800

    Added new members to user class / database functionality to work with new user object / updates to addPassengerRequest & acceptPassengers methods

--- app/src/main/java/chub45/benson/hitch/HitchDatabase.java
+++ app/src/main/java/chub45/benson/hitch/HitchDatabase.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Driver;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -35,38 +36,35 @@ public class HitchDatabase
     public void addUser(User user)
     {
         HashMap<String, String> userMap = makeUserMap(user);
-
-        DatabaseReference currentChild = usersRef.child(user.toString());
+        DatabaseReference currentChild = usersRef.child(user.getUid());
 
         currentChild.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()) {                                  // if the data was successfully added to the database
+                if(task.isSuccessful()) {
                     // Make toast or whatever you want
                 }
-                else{                                                      // if the data was not successfully added
+                else{
                     // Make burnt toast
                 }
             }
         });
+    }
 
+    private HashMap<String,String> makeUserMap(User user){
+        HashMap<String, String> userMap = new HashMap<String, String>();
+        userMap.put("uid", user.getUid());
+        userMap.put("fullName", user.getFullName());
+        userMap.put("city", user.getCity());
+        userMap.put("state", user.getState());
+        userMap.put("rideRequests", user.getRideRequests());
+        userMap.put("activeRides", user.getActiveRides());
+        return userMap;
     }
 
     public void addPost(Post post)
     {
-        HashMap<String, String> postMap = new HashMap<>();
-
-        postMap.put("departing_area", post.getdeparting_area());
-        postMap.put("destination", post.getdestination());
-        postMap.put("departure_time", post.getdeparture_time().toString());
-        postMap.put("available_spots", post.getavailable_spots().toString());
-        postMap.put("description", post.getdescription());
-        postMap.put("author_uid", post.getauthor());
-        postMap.put("author_email", post.getauthor_email());
-        postMap.put("post_id", post.get_post_id().toString());
-        postMap.put("potential_passengers", post.getpotential_passengers());
-        postMap.put("accepted_passengers", post.getaccepted_passengers());
-
+        HashMap<String, String> postMap = makePostMap(post);
         DatabaseReference currentChild = postsRef.child(post.get_post_id().toString());
 
         currentChild.setValue(postMap).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -81,50 +79,133 @@ public class HitchDatabase
         });
     }
 
-    private HashMap<String,String> makeUserMap(User user){
-        HashMap<String, String> userMap = new HashMap<String, String>();
-        userMap.put("username", user.toString());
-        userMap.put("email", user.getEmail());
-        userMap.put("fullName", user.getFullName());
-        return userMap;
+    public HashMap<String, String> makePostMap(Post post){
+        HashMap<String, String> postMap = new HashMap<>();
+        postMap.put("departing_area", post.getdeparting_area());
+        postMap.put("destination", post.getdestination());
+        postMap.put("departure_time", post.getdeparture_time().toString());
+        postMap.put("available_spots", post.getavailable_spots().toString());
+        postMap.put("description", post.getdescription());
+        postMap.put("author_uid", post.getauthor());
+        postMap.put("author_email", post.getauthor_email());
+        postMap.put("post_id", post.get_post_id().toString());
+        postMap.put("potential_passengers", post.getpotential_passengers());
+        postMap.put("accepted_passengers", post.getaccepted_passengers());
+        return postMap;
     }
 
-//    public HitchPost getPost(String username) { // return type will be a post
-//        /* query the database to find the postTitle and return the post object */
-//    }
+    public void addPassengerRequest(final String passengerUid, final String postID){
+        final DatabaseReference currentPostRef = postsRef.child(postID).child("potential_passengers");
+        final DatabaseReference currentUserRef = usersRef.child(passengerUid).child("rideRequests");
 
-    public void addPassengerRequest(final String passengerUID, String postID){
-        final DatabaseReference currentRef = postsRef.child(postID).child("potential_passengers");
-
-        currentRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        currentPostRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                     String passengers = (String) dataSnapshot.getValue();
-                    Log.d("CURRENT", "currval: " + passengers);
-                     if (passengers.isEmpty() && !passengers.contains(passengerUID)) {
-                         currentRef.setValue(passengerUID);
+                     if (passengers.isEmpty() && !passengers.contains(passengerUid)) {
+                         currentPostRef.setValue(passengerUid);
                       }
-                      else if(!passengers.isEmpty() && !passengers.contains(passengerUID)){
-                          currentRef.setValue(passengers + "|" + passengerUID);
+                      else if(!passengers.isEmpty() && !passengers.contains(passengerUid)){
+                          currentPostRef.setValue(passengers + "|" + passengerUid);
                       }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                     Log.d("FAILURE", "Could not add passenger request because: " + databaseError.getCode());
+                     Log.d("FAILURE", "Could not add to request to post because: " + databaseError.getCode());
+            }
+        });
+
+        currentUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String rides = (String) dataSnapshot.getValue();
+                if(rides.isEmpty()){
+                    currentUserRef.setValue(postID);
+                }
+                else if(!rides.isEmpty() && !rides.contains(postID)){
+                    currentUserRef.setValue(rides + "|" + postID);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("FAILURE", "Could not add to request to user because: " + databaseError.getCode());
             }
         });
     }
 
-    public void acceptPassengers(String postID){
-        final DatabaseReference currentRef = postsRef.child(postID);
-        currentRef.addListenerForSingleValueEvent(new ValueEventListener() {
+    public void acceptPassengers(final String postID){
+        final DatabaseReference currentPostRef = postsRef.child(postID);
+        currentPostRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String requestedPassengers = (String) dataSnapshot.child("potential_passengers").getValue();
+                Integer availSpots = Integer.parseInt((String) dataSnapshot.child("available_spots").getValue());
+
                 if(!requestedPassengers.isEmpty()) {
-                    currentRef.child("accepted_passengers").setValue(requestedPassengers);
-                    currentRef.child("potential_passengers").setValue("");
+                    int j = 0;
+                    ArrayList<String> passengers = new ArrayList<>(10);
+                    String temp = "";
+                    for(int i = 0; i < requestedPassengers.length(); i++){
+                        if(requestedPassengers.charAt(i) != '|'){
+                            temp += requestedPassengers.charAt(i);
+                        }
+                        else{
+                            passengers.add(j, temp);
+                            temp = "";
+                            j++;
+                        }
+                        if(i == requestedPassengers.length() - 1){
+                            passengers.add(j, temp);
+                        }
+                    }
+                    availSpots -= passengers.size();
+                    currentPostRef.child("accepted_passengers").setValue(requestedPassengers);
+                    currentPostRef.child("potential_passengers").setValue("");
+                    currentPostRef.child("available_spots").setValue(availSpots.toString());
+
+                    for(int k = 0; k < passengers.size(); k++){
+                        final DatabaseReference currentUserRef = usersRef.child(passengers.get(k));
+                        currentUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                String currentRideRequests = (String) dataSnapshot.child("rideRequests").getValue();
+                                String currentActiveRides = (String) dataSnapshot.child("activeRides").getValue();
+
+                                String temp0 = "";
+                                String temp1 = "";
+                                for(int i = 0; i < currentRideRequests.length(); i++){
+                                    if(currentRideRequests.contains(postID)){
+                                        int index = currentRideRequests.indexOf(postID);
+                                        if(index == 0 && currentRideRequests.length() != postID.length()){
+                                            temp1 = currentRideRequests.substring(postID.length()+1);
+                                        }
+                                        else if(index == currentRideRequests.length()-postID.length()){
+                                            temp1 = currentRideRequests.substring(0, index);
+                                        }
+                                        else{
+                                            temp0 = currentRideRequests.substring(0, index);
+                                            temp1 = currentRideRequests.substring(index + postID.length()+1);
+                                        }
+                                    }
+                                }
+                                currentUserRef.child("rideRequests").setValue(temp0 + temp1);
+
+                                String temp2 = "";
+                                if(currentActiveRides.isEmpty()){
+                                    temp2 = postID;
+                                }
+                                else{
+                                    temp2 = currentActiveRides + "|" + postID;
+                                }
+                                currentUserRef.child("activeRides").setValue(postID);
+                            }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                Log.d("FAILURE", "Could not accept passengers because: " + databaseError.getCode());
+                            }
+                        });
+                    }
                 }
             }
             @Override
@@ -134,15 +215,11 @@ public class HitchDatabase
         });
     }
 
-    public void removeUser(String username) {
-        usersRef.child(username).removeValue();
+    public void removeUser(String uid) {
+        usersRef.child(uid).removeValue();
     }
 
     public void removePost(String post_id){
         postsRef.child(post_id).removeValue();
-    }
-
-    public DatabaseReference getRoot(){
-        return rootRef;
     }
 }
